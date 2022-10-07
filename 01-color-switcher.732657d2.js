0e514af6dcb48133224d88e7f8be5d798b22b3d2
Siganova <100203224+Siganova@users.noreply.github.com>
2022-10-07 19:36:32 +0000

    Deploying to gh-pages from @ Siganova/goit-js-hw-09@987d7f4982be8fbfa61bb68088a534935cc04df2 🚀

---
+++ 01-color-switcher.732657d2.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),d=document.body;let r=null;e.setAttribute("disabled",!0),t.addEventListener("click",(()=>{r=setInterval((()=>{d.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.setAttribute("disabled",!0),e.removeAttribute("disabled")})),e.addEventListener("click",(()=>{clearInterval(r),t.removeAttribute("disabled"),e.setAttribute("disabled",!0)}));
+//# sourceMappingURL=01-color-switcher.732657d2.js.map
