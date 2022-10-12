f2f85a051422817e8b610c619bc93b7606d9296d
Nataly Siganova <siganova.tihanskaya@gmail.com>
2022-10-12 22:44:36 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,38 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form')
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault()
+  
+  const elements = e.currentTarget.elements;
+  const amount =  parseInt(elements['amount'].value, 10);
+  const delay =  parseInt(elements['delay'].value, 10);
+  const step =  parseInt(elements['step'].value, 10);
+  let currentDelay = delay;
+  for (let i = 0; i < amount; i++){
+    const promise = createPromise(i + 1, currentDelay);
+    promise.then((data) => {
+      Notiflix.Notify.success(`✅ Fulfilled promise ${data.position} in ${data.delay}ms`);
+    })
+    .catch((data) => {
+      Notiflix.Notify.failure(`❌ Rejected promise ${data.position} in ${data.delay}ms`);
+    })
+    currentDelay += step;
   }
+})
+
+function createPromise(position, delay) {
+
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({position, delay});
+      } else {
+        reject({position, delay});
+      }
+    }, delay);
+  });
 }
