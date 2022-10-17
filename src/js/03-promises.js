d79f216da7054991e66dc71d08e1a9f0bec069ff
Nataly Siganova <siganova.tihanskaya@gmail.com>
2022-10-17 22:33:54 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -4,23 +4,30 @@ const form = document.querySelector('.form')
 
 form.addEventListener("submit", (e) => {
   e.preventDefault()
-  
   const elements = e.currentTarget.elements;
-  const amount =  parseInt(elements['amount'].value, 10);
-  const delay =  parseInt(elements['delay'].value, 10);
-  const step =  parseInt(elements['step'].value, 10);
+  const amount = parseInt(elements['amount'].value, 10);
+  const delay = parseInt(elements['delay'].value, 10);
+  const step = parseInt(elements['step'].value, 10);
   let currentDelay = delay;
-  for (let i = 0; i < amount; i++){
+  for (let i = 0; i < amount; i++) {
     const promise = createPromise(i + 1, currentDelay);
     promise.then((data) => {
       Notiflix.Notify.success(`✅ Fulfilled promise ${data.position} in ${data.delay}ms`);
+      clearForm(i + 1, amount, e);
     })
-    .catch((data) => {
-      Notiflix.Notify.failure(`❌ Rejected promise ${data.position} in ${data.delay}ms`);
-    })
+      .catch((data) => {
+        Notiflix.Notify.failure(`❌ Rejected promise ${data.position} in ${data.delay}ms`);
+        clearForm(i + 1, amount, e);
+      })
     currentDelay += step;
   }
-})
+});
+
+function clearForm(position, amount, event){
+  if(position === amount){
+    event.target.reset();
+  }
+}
 
 function createPromise(position, delay) {
 
